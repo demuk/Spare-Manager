41db688ce82f9be3cd8de821d12105d124dbfa2e
kiptoo <denysmutay@gmail.com>
2021-08-23 12:57:24 +0300

    added form for receiving login form data

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,21 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 
 
-@app.route('/home', methods=['POST','GET'])
+@app.route('/index', methods=['POST','GET'])
 @app.route('/', methods=['POST', 'GET'])
-def home():
+def index():
     title = 'HOME'
-    return render_template('home.html', title=title)
+    return render_template('index.html', title=title)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
