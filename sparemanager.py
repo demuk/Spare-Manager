c808ac0d7b7669d235f4334ee80353fc3d8e6aa7
kiptoo <denysmutay@gmail.com>
2021-08-25 15:12:30 +0300

    updated login.html

--- sparemanager.py
+++ sparemanager.py
@@ -1,1 +1,8 @@
-from app import app
+from app import app, db
+from app.models import User, Spare
+
+
+@app.shell_context_processor
+def make_shell_conext():
+    return {'db': db, 'User': User, 'Spare': Spare}
+
