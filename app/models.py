0cf0d8f1adf7344ba7801937cc40b8563d49bd44
kiptoo <denysmutay@gmail.com>
2021-08-25 15:09:51 +0300

    added password hashing and enabled verifications

--- app/models.py
+++ app/models.py
@@ -1,12 +1,22 @@
 from app import db
 from datetime import datetime
+from flask_login import UserMixin
+from app import login
 
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
     def __repr__(self):
@@ -26,4 +36,7 @@ class Spare(db.Model):
     def __repr__(self):
         return '<Spare {}>'.format(self.body)
 
-    
+
+@login.user_loader
+def load_user(id):
+    return User.query(int(id))
