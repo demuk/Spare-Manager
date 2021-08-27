957db2d5397ed099df526461655ef4d433811d04
kiptoo <denysmutay@gmail.com>
2021-08-27 12:14:44 +0300

    added flask form for capturing data from form fields

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm, RegisterForm
+from app.forms import LoginForm, RegisterForm, AddSpareForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import Spare, User
 from werkzeug.urls import url_parse
